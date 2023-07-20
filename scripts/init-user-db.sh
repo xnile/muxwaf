4f93daa8cb6624f22e8c3937ac8ff16fc5e060e7
xnile <imxnile@gmail.com>
2023-07-20 17:42:33 +0800

    数据库初始化脚本增加自定义类型
    Signed-off-by: xnile <imxnile@gmail.com>

--- scripts/init-user-db.sh
+++ scripts/init-user-db.sh
@@ -5,4 +5,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER muxwaf WITH PASSWORD 'muxwaf@password';
     CREATE DATABASE muxwaf;
     GRANT ALL PRIVILEGES ON DATABASE muxwaf TO muxwaf;
+    CREATE TYPE origin_protocol  AS ENUM ('http', 'https', 'follow');
 EOSQL
